24ed9d731945ded034fd00b6f45be95ec30a484a
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2019-03-24 20:32:14 -0600

    arreglos

--- src/Componentes/Automoviles.js
+++ src/Componentes/Automoviles.js
@@ -26,62 +26,62 @@ export default class Automoviles extends Component{
               {
                 title: "BMW X5",
                 subtitle: "50$ por día",
-                picture: icon10
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fbmwx5.jpg?alt=media&token=52f63268-9cad-4d03-b90a-4ac8d6bf361b"
               },
               {
                 title: "Ford Fiesta",
                 subtitle: "15$ por día",
-                picture: icon4
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Ffordfiesta.jpg?alt=media&token=8af7a9bf-c630-466e-a628-29c17d4ee4e9"
               },
               {
                 title: "Ford Focus",
                 subtitle: "20$ por día",
-                picture: icon9
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Ffordfocus.jpg?alt=media&token=cd88221a-2d6f-4e02-ac3a-a5b2e4a628cd"
               },
               {
                 title: "Honda Accord",
                 subtitle: "25$ por día",
-                picture: icon1
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fhondaaccord.jpg?alt=media&token=5babe88c-cf24-405a-b9a0-9d824a7072b7"
               },
               {
                 title: "Honda CRV",
                 subtitle: "35$ por día",
-                picture: icon5
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fhondacrv.jpg?alt=media&token=c600ef83-561b-49bd-90b1-67ffd5d88c8f"
               },
               {
                 title: "Honda HRV",
                 subtitle: "35$ por día",
-                picture: icon8
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fhondahrv.jpg?alt=media&token=fb9cd00a-4057-46ee-a453-5114511d1b17"
               },
               {
                 title: "Kia Sorento",
                 subtitle: "30$ por día",
-                picture: icon2
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fkia.jpg?alt=media&token=3c7a7f45-11cf-4641-8aec-8b1b2ced6eb3"
               },
               {
                 title: "Kia Picanto",
                 subtitle: "10$ por día",
-                picture: icon6
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fkiapicanto.jpg?alt=media&token=ab2a01ea-6c03-444b-b8fd-d80fa8b58d7d"
               },
               {
                 title: "Kia Rio",
                 subtitle: "20$ por día",
-                picture: icon3
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fkiario.jpg?alt=media&token=329d22c2-78d1-4dbe-9ee2-b044dde0a538"
               },
               {
                 title: "Mitsubishi ASX",
                 subtitle: "25$ por día",
-                picture: icon7
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fmitshubishiasx.jpg?alt=media&token=29483f88-3ac7-49e3-a041-5e785b8a2259"
               },
               {
                 title: "Range Rover Evoque",
                 subtitle: "60$ por día",
-                picture: icon11
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Frangeroverevoque.jpg?alt=media&token=35bc7914-855f-4a37-a862-c100157ec7f8"
               },
               {
                 title: "Toyota Land Cruiser Prado",
                 subtitle: "40$ por día",
-                picture: icon12
+                picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Ftoyotalandcruiser.jpg?alt=media&token=3d321286-fb9e-4a64-913a-e484388d7f5d"
               },
             ]
           };
--- src/Componentes/Bikes.js
+++ src/Componentes/Bikes.js
@@ -21,32 +21,32 @@ export default class UserMainPg extends React.Component {
         {
           title: "Honda African Twin",
           subtitle: "13$ por día",
-          picture: icon1
+          picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fhondaafricantwin.jpg?alt=media&token=0d385069-2c36-48e0-bceb-d3e158885548"
         },
         {
           title: "Honda Gold Wing",
           subtitle: "13$ por día",
-          picture: icon2
+          picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fhondagoldwing.jpg?alt=media&token=bcf428b4-ee58-46f0-8457-196eb5a73ec0"
         },
         {
           title: "Honda Monkey",
           subtitle: "12$ por día",
-          picture: icon3
+          picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fhondamonkey.jpg?alt=media&token=8f1a10ef-5aa0-4590-803a-960e13fa992a"
         },
         {
           title: "Yamaha Tracer 900",
           subtitle: "10$ por día",
-          picture: icon4
+          picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fyamahatracer900.jpg?alt=media&token=c7953d4d-9116-49db-bbc0-98437b37232f"
         },
         {
           title: "Vespa",
           subtitle: "8$ por día",
-          picture: icon5
+          picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fvespa.jpg?alt=media&token=291344ed-ba38-4780-a26c-d6768c39a779"
         },
         {
           title: "BMW R Nine Tracer",
           subtitle: "20$ por día",
-          picture: icon6
+          picture: "https://firebasestorage.googleapis.com/v0/b/connectedcar-12d9e.appspot.com/o/Pictures%2Fbmwrninetracer.jpg?alt=media&token=f3b7f5e5-fdea-48cd-929a-3d2603cee176"
         }
       ]
     };
